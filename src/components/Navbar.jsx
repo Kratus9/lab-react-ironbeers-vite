444468e4b6e6ade811b5a82b8ba05d9a339feb73
Mark <marcboluda@hotmail.com>
2023-08-25 18:20:43 +0200

    Iteration 3 done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
+import homeIcon from '../assets/home-icon.png';
 
 function Navbar() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
+        <nav className='navbar-link'>
+            <Link to="/"><img src={ homeIcon } alt="Home" /></Link>
         </nav>
     )
 }
